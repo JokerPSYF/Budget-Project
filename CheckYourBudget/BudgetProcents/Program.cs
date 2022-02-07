00f4cd62ef2db711098533b4473806fe5368f644
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-07 20:17:41 +0200

    updated

--- CheckYourBudget/BudgetProcents/Program.cs
+++ CheckYourBudget/BudgetProcents/Program.cs
@@ -9,74 +9,76 @@ namespace BudgetTest
             //ПРОГРАМА КОЯТО ЩЕ ПОКАЖЕ ПО ПРОЦЕНТИ ЗА КАКВО СИ ХАЧРИШ ПАРИТЕ НАЙ-МНОГО В ДАДЕНИЯ МЕСЕЦ
 
             //INPUT
-            Console.Write("Put your salary: "); double salary = double.Parse(Console.ReadLine());
+            Console.Write("Put your salary: "); decimal salary = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for Internet and TV: "); double TV = double.Parse(Console.ReadLine());
+            Console.Write("Money for Internet and TV: "); decimal TV = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for food: "); double food = double.Parse(Console.ReadLine());
+            Console.Write("Money for food: "); decimal food = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for go out: "); double goOut = double.Parse(Console.ReadLine());
+            Console.Write("Money for go out: "); decimal goOut = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for home stuf: "); double homeStuf = double.Parse(Console.ReadLine());
+            Console.Write("Money for home stuf: "); decimal homeStuf = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for clothes: "); double clothes = double.Parse(Console.ReadLine());
+            Console.Write("Money for clothes: "); decimal clothes = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for gas: "); double gas = double.Parse(Console.ReadLine());
+            Console.Write("Money for gas: "); decimal gas = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for dad: "); double dad = double.Parse(Console.ReadLine());
+            Console.Write("Money for dad: "); decimal dad = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for hobby: "); double hobby = double.Parse(Console.ReadLine());
+            Console.Write("Money for hobby: "); decimal hobby = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for car: "); double car = double.Parse(Console.ReadLine());
+            Console.Write("Money for car: "); decimal car = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for cat: "); double cat = double.Parse(Console.ReadLine());
+            Console.Write("Money for cat: "); decimal cat = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for electricity and water: "); double electricity = double.Parse(Console.ReadLine());
+            Console.Write("Money for electricity and water: "); decimal electricity = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for cinema: "); double cinema = double.Parse(Console.ReadLine());
+            Console.Write("Money for cinema: "); decimal cinema = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for cut hair: "); double cutHair = double.Parse(Console.ReadLine());
+            Console.Write("Money for cut hair: "); decimal cutHair = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for games: "); double games = double.Parse(Console.ReadLine());
+            Console.Write("Money for games: "); decimal games = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Money for your future: "); double future = double.Parse(Console.ReadLine());
+            Console.Write("Money for your future: "); decimal future = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Money for your health: "); decimal health = decimal.Parse(Console.ReadLine());
 
 
             //LOGIC
-            double forCar = gas + car;
-            double forGames = games;
-            double forHome = TV + homeStuf + dad + cat + electricity;
-            double forFood = food;
-            double forClothes = clothes + cutHair;
-            double forFun = goOut + cinema;
-            double forHobby = hobby;
-            double forFuture = future;
-            double sum = forCar + forGames + forHome + forFood + forClothes + forFun + forHobby + forFuture;
+            decimal forCar = gas + car;
+            decimal forGames = games;
+            decimal forHome = TV + homeStuf + dad + cat + electricity;
+            decimal forFood = food;
+            decimal forClothes = clothes + cutHair;
+            decimal forFun = goOut + cinema;
+            decimal forHobby = hobby;
+            decimal forFuture = future;
+            decimal sum = forCar + forGames + forHome + forFood + forClothes + forFun + forHobby + forFuture + health;
 
-            double forNeeds = forCar + forHome + forFood + forClothes;
-            double forInvestment = forFuture;
-            double forWant = forGames + forFun + forHobby;
+            decimal forNeeds = forCar + forHome + forFood + forClothes + health;
+            decimal forInvestment = forFuture;
+            decimal forWant = forGames + forFun + forHobby;
 
-            double percentConverterForCar = GiveMeProcent(salary, forCar);
-            double percentConverterForGames = GiveMeProcent(salary, forGames); ;
-            double percentConverterForHome = GiveMeProcent(salary, forHome);
-            double percentConverterForFood = GiveMeProcent(salary, forFood);
-            double percentConverterForClothes = GiveMeProcent(salary, forClothes);
-            double percentConverterForFun = GiveMeProcent(salary, forFun);
-            double percentConverterForHobby = GiveMeProcent(salary, forHobby);
-            double percentConverterForFuture = GiveMeProcent(salary, forFuture);
-            double percentConverterForNeeds = GiveMeProcent(salary, forNeeds);
-            double percentConverterForInvestment = GiveMeProcent(salary, forInvestment);
-            double percentConverterForWant = GiveMeProcent(salary, forWant);
-            double residue = salary - sum;
+            decimal percentConverterForCar = GiveMeProcent(salary, forCar);
+            decimal percentConverterForGames = GiveMeProcent(salary, forGames); ;
+            decimal percentConverterForHome = GiveMeProcent(salary, forHome);
+            decimal percentConverterForFood = GiveMeProcent(salary, forFood);
+            decimal percentConverterForClothes = GiveMeProcent(salary, forClothes);
+            decimal percentConverterForFun = GiveMeProcent(salary, forFun);
+            decimal percentConverterForHobby = GiveMeProcent(salary, forHobby);
+            decimal percentConverterForFuture = GiveMeProcent(salary, forFuture);
+            decimal percentConverterForNeeds = GiveMeProcent(salary, forNeeds);
+            decimal percentConverterForInvestment = GiveMeProcent(salary, forInvestment);
+            decimal percentConverterForWant = GiveMeProcent(salary, forWant);
+            decimal residue = salary - sum;
 
             //OUTPUT
             Console.WriteLine();
 
             Console.WriteLine($"Rule : 50/40/10 \n50% ({(salary/2):f2} лв.) for needs (food, house, transport, debt) \n" +
-                $"40% ({(salary*0.40):f2} лв.)for investment (emergency fund, self-education," +
+                $"40% ({(salary*(decimal) 0.40):f2} лв.)for investment (emergency fund, self-education," +
                 $" stocks/ETFs, rental, properties\n" +
-                $"10% ({(salary*0.10):f2} лв.) for wants " +
+                $"10% ({(salary*(decimal) 0.10f):f2} лв.) for wants " +
                 $"(restaurants, entertainment, vacations, luxuary items, electronics)");
 
             Console.WriteLine($"Money for car:{percentConverterForCar:f2}% ");
@@ -104,9 +106,9 @@ namespace BudgetTest
                 $" Wants:{percentConverterForWant:f2}%({(percentConverterForWant / 100 * salary):f2}лв.)");
         }
 
-        private static double GiveMeProcent(double salary, double expenditure)
+        private static decimal GiveMeProcent(decimal salary, decimal expenditure)
         {
-            double procent = expenditure / salary * 100;
+            decimal procent = expenditure / salary * 100;
             return procent;
         }
     }
